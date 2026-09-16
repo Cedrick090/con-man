47f9e45c803d1beba04677d2467edc2389ac4773
Cedrick090 <alabadocedrickloid@gmail.com>
2026-09-16 22:59:58 +0800

    1
    Updated Firebase configuration with specific project details.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDw-0SIDO6F0bz_Y3mhRonwJUaEOZcD2EA",
+  authDomain: "ionic-conam.firebaseapp.com",
+  databaseURL: "https://ionic-conam-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ionic-conam",
+  storageBucket: "ionic-conam.firebasestorage.app",
+  messagingSenderId: "68415464644",
+  appId: "1:68415464644:web:a6caf2415999a6334e1591",
+  measurementId: "G-5RLE0XDEN5"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+const analytics = getAnalytics(app);
